0487fa7d76d1f737dfa6a13b55e7f4c08af12d70
Tony S Yu <tsyu80@gmail.com>
2014-11-06 22:25:34 -0600

    Travis, I hate you

--- snakeviz/tests/test_upload.py
+++ snakeviz/tests/test_upload.py
@@ -43,10 +43,8 @@ def test_stats_to_tree_dict():
     """)
 
     with temp_pstats_tree('simple_func()', locals(), 'simple_func') as root:
-        print('-'*80)
         graph = ptree_to_call_graph(root)
         assert_call_graphs_match(graph, expected_graph)
 
         graph = ptree_to_call_graph(root, max_depth=2)
         assert_call_graphs_match(graph, {'simple_func': ['len', 'sub_func']})
-        assert 0
